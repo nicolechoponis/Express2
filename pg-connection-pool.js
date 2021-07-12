289f621be4e3dabeae7b67133560166f6eead22d
Nicole Choponis <nicolechoponis@gmail.com>
2021-07-12 19:14:06 -0400

    last commit

--- pg-connection-pool.js
+++ pg-connection-pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 const credentials = new Pool({
   user: "postgres",
-  password: "Mosc@to09SQL",
+  password: "insert password here",
   host: "localhost",
   port: 5432,
   database: "ExpressShopDB",
